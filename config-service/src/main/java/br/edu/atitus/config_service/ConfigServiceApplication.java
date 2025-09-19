7a4652fbee58b899f517b8ae7d7cd83cb24e7165
Pedro Segatto <pedrobsegatto@gmail.com>
2025-09-19 19:59:35 -0300

    Atualizacao

---
+++ config-service/src/main/java/br/edu/atitus/config_service/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
